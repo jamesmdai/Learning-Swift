9d6a962c796c79c32683b7a3d690ff8d104a3a38
James Dai <jamesmdai@gmail.com>
2020-08-12 11:47:42 -0400

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by James Dai on 8/12/20.
+//  Copyright © 2020 James Dai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
